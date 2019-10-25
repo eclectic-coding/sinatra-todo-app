bcb6d2055485f1b571a8c74139700d75ebe3ae89
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-10-24 21:53:03 -0400

    Add DOTENV and Secure password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,28 @@
 require './config/environment'
+require 'securerandom'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'todorandom506'
+    # set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def signed_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
   end
 
 end
