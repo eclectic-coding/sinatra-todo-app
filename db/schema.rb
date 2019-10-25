c23ae2f33d1c7bad7db36b2624beafa9f44f6373
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-10-25 12:38:37 -0400

    Set up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191024010409) do
+ActiveRecord::Schema.define(version: 20191025161828) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string  "title"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
