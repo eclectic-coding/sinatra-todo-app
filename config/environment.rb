ea043ada02b351acf883459c12a1f376550263a1
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-01-24 13:18:16 -0500

    Update environment for heroku deploy.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,19 @@
 ENV['SINATRA_ENV'] ||= 'development'
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require
 
+configure :development do
+  ENV['SINATRA_ENV'] ||= 'development'
+
+  require 'bundler/setup'
+  Bundler.require(:default, ENV['SINATRA_ENV'])
+
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+end
 
 configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
